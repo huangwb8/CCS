927a225fa40dcd9446f972603ff7e20f902ea36e
Weibin Huang <654751191@qq.com>
2023-12-25 13:43:17 +0800

    Routine Update
    + Create prediction function
    + Optimize plot for visualization of t-SNE results

--- R/ccs.R
+++ R/ccs.R
@@ -32,9 +32,10 @@ setClass("CCS",
              max.nc= numeric(),
              model.dir = character()
            ),
-           Data=list(
+           Data = list(
              Probability = list(),
-             CCS = integer()
+             CCS = integer(),
+             scaller = list()
            ),
            Plot = ggplot()
          )
@@ -55,6 +56,7 @@ setClass("CCS",
 #' @importFrom Rtsne Rtsne
 #' @import ggplot2
 #' @import GSClassifier
+#' @import xgboost
 #' @return a CCS object
 #' @author Weibin Huang<\email{hwb2012@@qq.com}>
 #' @examples
@@ -148,6 +150,8 @@ ccs <- function(
     min.nc = 2
     max.nc= 10
     dimension = c(2,2)
+    perplexity = 30
+    theta = 0.3
 
     # Model parameters
     params <- list(
@@ -278,70 +282,131 @@ ccs <- function(
   d1 <- data_for_tSNE(res2, verbose);
 
 
-  # Dimensionality reduction - Level 1
-  d2 <- d1$cleaned$data
-  reference <- Fastextra(colnames(d2), '[|]', 1)
-  d3 <- drCCSProbability(
-    d2,
-    reference = reference,
-    dims = dimension[1],
-    perplexity = perplexity,
-    theta = theta,
-    seed = seeds[4],
-    verbose = verbose
-  )
+  # Dimensionality reduction
+  if(T){
+    # Dimensionality reduction - Level 1
+    d2 <- d1$cleaned$data
+    reference <- Fastextra(colnames(d2), '[|]', 1)
+    d3 <- drCCSProbability(
+      d2,
+      reference = reference,
+      dims = dimension[1],
+      perplexity = perplexity,
+      theta = theta,
+      seed = seeds[4],
+      verbose = verbose
+    )
 
-  # Dimensionality reduction - Level 2
-  d4 <- drCCSProbability(
-    d3,
-    reference = NULL,
-    dims = dimension[2],
-    perplexity = perplexity,
-    theta = theta,
-    seed = seeds[5],
-    verbose = verbose
-  )
-
-  # Plot
-  p <- ggplot(d4, aes(x = `all|D1`, y = `all|D2`)) +
-    geom_point() +
-    ggtitle("t-SNE Visualization")
-  # win.graph(); print(p)
+    # Dimensionality reduction - Level 2
+    d4 <- drCCSProbability(
+      d3,
+      reference = NULL,
+      dims = dimension[2],
+      perplexity = perplexity,
+      theta = theta,
+      seed = seeds[5],
+      verbose = verbose
+    )
+  }
 
 
   # CCS subtypes
-  if(nrow(d4) > 500){
-    set.seed(178);
-    numComplete <- NbClust2(
-      data = d4[sample(1:nrow(d4), 500),],
-      distance = "euclidean",
-      min.nc = min.nc,
-      max.nc= max.nc,
-      method = "ward.D2",
-      index = "all",
-      verbose = verbose
+  if(T){
+    if(nrow(d4) > 500){
+      set.seed(seeds[6]);
+      numComplete <- NbClust2(
+        data = d4[sample(1:nrow(d4), 500),],
+        distance = "euclidean",
+        min.nc = min.nc,
+        max.nc= max.nc,
+        method = "ward.D2",
+        index = "all",
+        verbose = verbose
+      )
+      dis <- dist(d4, method = "euclidean")
+      hc <- hclust(dis, method =  "ward.D2")
+      y2 <- cutree(hc, length(unique(numComplete$Best.partition)))
+    } else {
+      set.seed(seeds[6]);
+      numComplete <- NbClust2(
+        data = d4,
+        distance = "euclidean",
+        min.nc = min.nc,
+        max.nc= max.nc,
+        method = "ward.D2",
+        index = "all",
+        verbose = verbose
+      )
+      y2 <- numComplete$Best.partition
+    }
+    index <- match(d1$raw$md5, d1$cleaned$md5)
+    y3 <- NULL
+    for(i in 1:length(index)){
+      y3 <- c(y3, y2[index[i]])
+    }
+    names(y3) <- as.character(res$SampleIDs)
+  }
+
+
+  # Plot
+  if(verbose) LuckyVerbose('Dimensionality reduction visulization via ggplot2...')
+  if(T){
+    dat_plot <- cbind(d4, CCS = paste('CCS',y2,sep = ''))
+    size = 15 # plot size
+    p <- ggplot(dat_plot, aes(x = `all|D1`, y = `all|D2`, color = CCS)) +
+      geom_point() +
+      labs(title = "t-SNE Visualization") +
+      theme_bw() +
+      theme(
+        axis.text = element_text(size = size/15*12,colour = "black",face = "bold"),
+        plot.title = element_text(size = size,colour = "black",face = "bold", hjust = 0.5),
+        axis.title.x = element_text(size = size,colour = "black",face = "bold"),
+        axis.title.y = element_text(size = size,colour = "black",face = "bold"),
+        legend.text = element_text(size = size/15*12,colour = "black",face = "bold"),
+        legend.title = element_text(size = size/15*12,colour = "black",face = "bold"),
+        legend.position='right',
+        strip.background = element_rect(fill="white"),
+        strip.text = element_text(size = size/15*12,colour = "black",face = "bold")); # win.graph(10,10); print(p)
+  }
+
+
+  # Subtype Caller
+  if(verbose) LuckyVerbose('Build subtype caller ...')
+  path_scaller <- paste0(model.dir, '/scaller.rds')
+  if(!file.exists(path_scaller)){
+
+    nSubtype <- length(unique(y2))
+    dtrain <- xgb.DMatrix(d2, label = y2-1)
+
+    # xgboost cv for best interation exploration
+    set.seed(seeds[7])
+    cvRes <- xgb.cv(
+      params = params_xg2,
+      data = dtrain,
+      nrounds=params_xg$nrounds,
+      nfold=params_xg$nfold,
+      num_class = nSubtype,
+      early_stopping_rounds=10,
+      objective = "multi:softmax",
+      verbose = 0
     )
-    dis <- dist(d4, method = "euclidean")
-    hc <- hclust(dis, method =  "ward.D2")
-    y2 <- cutree(hc, length(unique(numComplete$Best.partition)))
+    best_iteration <- cvRes$best_iteration
+
+    # xgboost via best interation
+    set.seed(seeds[7])
+    scaller <- xgboost(
+      params = params_xg2,
+      data = dtrain,
+      nrounds = best_iteration,
+      num_class = nSubtype,
+      objective = "multi:softmax",
+      verbose = 0
+    )
+    saveRDS(scaller, path_scaller)
   } else {
-    numComplete <- NbClust2(
-      data = d4,
-      distance = "euclidean",
-      min.nc = min.nc,
-      max.nc= max.nc,
-      method = "ward.D2",
-      index = "all",
-      verbose = verbose
-    )
-    y2 <- numComplete$Best.partition
+    if(verbose) LuckyVerbose('The subtype caller exists. Use it!')
+    scaller <- readRDS(path_scaller)
   }
-  index <- match(d1$raw$md5, d1$cleaned$md5)
-  y3 <- NULL
-  for(i in 1:length(index)){
-    y3 <- c(y3, y2[index[i]])
-  }
-  names(y3) <- as.character(res$SampleIDs)
 
   # Output
   # l <- list(
@@ -387,10 +452,124 @@ ccs <- function(
         d2 = d3,
         d3 = d4
       ),
-      CCS = y3
+      CCS = y3,
+      scaller = scaller
     ),
     Plot = p
   )
   if(verbose) LuckyVerbose('All done!')
   return(l)
 }
+
+
+#' @title CCS-method
+#' @name CCS-method
+#' @description \code{predict} method for \code{CCS} class
+#' @param object a CCS object
+#' @inheritParams ccs
+#' @inheritParams GSClassifier::parCallEnsemble
+#' @import GSClassifier
+#' @import xgboost
+#' @importFrom luckyBase Fastextra
+#' @return A list object with CCS subtype prediction.
+#' @seealso \code{\link{ccs}}.
+#' @author Weibin Huang<\email{hwb2012@@qq.com}>
+#' @examples
+#' ccs_pred <- predict(resCCS, model.dir = "./ccs/project_01")
+#' @export
+predict.CCS <- function(
+    object, X,
+    model.dir = NULL,
+    verbose = T,
+    numCores = 4){
+
+  # Test
+  if(F){
+    library(luckyBase); library(GSClassifier)
+    work.space = 'E:/RCloud/RFactory/CCS/test/ccs/project_01'
+    object = readRDS(paste0(work.space, '/', 'resCCS.rds'))
+    X = data[["cancer_1"]][["cohort.1.1"]][["expr"]][,1:5]
+    model.dir = './test/ccs/project_01'
+    numCores = 4
+    verbose = F
+  }
+
+  # Model parameters
+  if(is.null(model.dir)){
+    model.dir = object@Repeat$model.dir
+  }
+  geneAnnotation = object@Repeat$geneAnnotation
+  geneSet = object@Repeat$geneSet
+  geneid = object@Repeat$geneid
+  scaller = object@Data$scaller
+
+  # Expression matrix
+  X <- GSClassifier:::rightX(X)
+  nSample <- ncol(X)
+
+  # Call CCS probability
+  path_models <- list.files(path = model.dir, pattern = 'modelFit.rds$', recursive = T, full.names = T)
+  X_CCSprobability <- NULL
+  for(i in 1:length(path_models)){ # i=1
+
+    # A GSClassifier model
+    path_models.i <- path_models[i]
+    modelFit <- readRDS(path_models.i)
+    name_model1 <- rev(Fastextra(path_models.i, '[/]'))
+    cohort_model1 <- name_model1[2]; cancertype_model1 <- name_model1[3]
+    LuckyVerbose('Load model of ', cancertype_model1, ' - ', cohort_model1 ," cohort : ")
+
+    # Call CCS probability
+    if(nSample > 100){
+      X_CCSprobability.i <- parCallEnsemble(
+        X = X,
+        ens = modelFit$Model,
+        geneAnnotation = geneAnnotation,
+        geneSet = geneSet,
+        geneid = geneid,
+        scaller = NULL,
+        subtype = NULL,
+        verbose = F,
+        numCores = numCores
+      )
+    } else {
+      X_CCSprobability.i <- callEnsemble(
+        X = X,
+        ens = modelFit$Model,
+        geneAnnotation = geneAnnotation,
+        geneSet = geneSet,
+        geneid = geneid,
+        scaller = NULL,
+        subtype = NULL,
+        verbose = F
+      )
+    }
+
+    colnames(X_CCSprobability.i)[3:ncol(X_CCSprobability.i)] <- paste(cancertype_model1, cohort_model1,  colnames(X_CCSprobability.i)[3:ncol(X_CCSprobability.i)], sep = '|')
+    res.i <- t(apply(X_CCSprobability.i[-c(1,2,3)], 1, softmax))
+
+    # Merge results
+    if(i == 1){
+      X_CCSprobability <- res.i
+    } else {
+      X_CCSprobability <- cbind(X_CCSprobability, res.i)
+    }
+  }
+  X_CCS_Pred <- predict(scaller, X_CCSprobability) + 1
+  names(X_CCS_Pred) <- colnames(X)
+
+  # Output
+  l <- list(
+    X = X,
+    model.dir = model.dir,
+    CCS = list(
+      Probability = cbind(SampleIDs = colnames(X), as.data.frame(X_CCSprobability)),
+      Prediction = X_CCS_Pred
+    )
+  )
+  return(l)
+}
+
+
+
+
