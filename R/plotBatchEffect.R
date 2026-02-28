27a45f050d234507e2add82f2bf1679b8f63cc25
Bensz Conan <35643122+huangwb8@users.noreply.github.com>
2026-03-01 07:30:03 +0800

    refactor(plotBatchEffect): 适配 macOS 开发环境并优化代码风格
    - 添加跨平台路径检测（支持 Darwin 系统）
    - 规范化代码风格（单引号改双引号、空格标准化）
    - 优化图形绘制方式（使用 ggplot + theme_void 添加坐标轴标题）

--- R/plotBatchEffect.R
+++ R/plotBatchEffect.R
@@ -1,5 +1,3 @@
-
-
 setGeneric("plotBatchEffect", function(object, ...) {
   standardGeneric("plotBatchEffect")
 })
@@ -14,19 +12,18 @@ setGeneric("plotBatchEffect", function(object, ...) {
 #' @exportMethod plotBatchEffect
 setMethod(
   "plotBatchEffect",
-  signature(object='CCS'),
-  function(
-    object,
-    data
-  ){
-
+  signature(object = "CCS"),
+  function(object,
+           data) {
     # Test
-    if(F){
+    if (F) {
       library(luckyBase)
-      Plus.library(c('tidyr','dplyr','plyr','reshape2','purrr','GSClassifier','ggplot2','cowplot'))
-      object <- readRDS('E:/iProjects/RCheck/GSClassifier/routine01/ccs/PADv20240810_variant_01/resCCS_PADv20240810.rds')
+      Plus.library(c("tidyr", "dplyr", "plyr", "reshape2", "purrr", "GSClassifier", "ggplot2", "cowplot"))
+      # 跨平台路径适配
+      root_path <- ifelse(Sys.info()["sysname"] == "Darwin", "/Volumes/2T01/winE", "E:")
+      object <- readRDS(file.path(root_path, "iProjects/RCheck/GSClassifier/test02/ccs/PADv20240810/prediction/PanIMTv20240726_legacy_02/resCCS_PADv20240810.rds"))
       mt <- object@Data$Probability$d1
-      data <- readRDS('E:/iProjects/CCS_Data/report/DataListForCCS_GEO+cBioPortal+UCXCXenav20240809_PADv20240810.rds')[c(1,2)]
+      data <- readRDS(file.path(root_path, "iProjects/CCS_Data/report/DataListForCCS_GEO+cBioPortal+UCXCXenav20240809_PADv20240810.rds"))[c(1, 2)]
       # data[['STAD']][['GSE21983']] <- NULL
     }
 
@@ -40,8 +37,8 @@ setMethod(
     # Alignment
     coSample <- intersect(rownames(mt_1), rownames(mt_2))
     mt_l <- list()
-    mt_l[[1]] <- mt_1[coSample,]
-    mt_l[[2]] <- mt_2[coSample,]
+    mt_l[[1]] <- mt_1[coSample, ]
+    mt_l[[2]] <- mt_2[coSample, ]
 
     # Plot data
     mt_l_plot <- llply(mt_l, function(mt) plotOneBatchEffect(object, data, mt))
@@ -55,19 +52,27 @@ setMethod(
 
     # Plot
     p <- plot_grid(
-      mt_l_plot[[1]][['Tissue']] + labs(title = 'Tissue-level') + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5)),
-      mt_l_plot[[1]][['Cohort']] + labs(title = 'Cohort-level') + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5)),
-      mt_l_plot[[2]][['Tissue']] + theme(axis.title = element_blank()),
-      mt_l_plot[[2]][['Cohort']] + theme(axis.title = element_blank()),
-      align = "h", ncol = 2, rel_widths = c(0.5, 0.5),rel_heights = c(0.5,0.5)
+      mt_l_plot[[1]][["Tissue"]] + labs(title = "Tissue-level") + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5)),
+      mt_l_plot[[1]][["Cohort"]] + labs(title = "Cohort-level") + theme(axis.title = element_blank(), plot.title = element_text(hjust = 0.5)),
+      mt_l_plot[[2]][["Tissue"]] + theme(axis.title = element_blank()),
+      mt_l_plot[[2]][["Cohort"]] + theme(axis.title = element_blank()),
+      align = "h", ncol = 2, rel_widths = c(0.5, 0.5), rel_heights = c(0.5, 0.5)
     )
-    x_axis_title <- ggdraw() + draw_label("Sample", x = 0.5, y = 0.5) # fontface = "bold",
-    p2 <-  plot_grid(
-      p, x_axis_title,
-      ncol = 1,
-      rel_heights = c(0.95, 0.05)
+    # 添加坐标轴标题（使用 ggplot + theme_void 更稳健）
+    x_axis_title <- ggplot() +
+      annotate("text", x = 0.5, y = 0.5, label = "Sample", size = 4) +
+      theme_void()
+    y_axis_title <- ggplot() +
+      annotate("text", x = 0.5, y = 0.5, label = "Expression", angle = 90, size = 4) +
+      theme_void()
+
+    # 组合图形：先绑定 x 轴标题，再绑定 y 轴标题
+    p2 <- plot_grid(
+      y_axis_title,
+      plot_grid(p, x_axis_title, ncol = 1, rel_heights = c(0.95, 0.05)),
+      ncol = 2, rel_widths = c(0.02, 0.98)
     )
-    # tiff(paste0('E:/iProjects/RCheck/GSClassifier/routine01/test/PADv20240810_plotBatchEffect_test01.tif'), width = 40, height = 22, res = 300, units = "cm"); print(p2); dev.off()
+    # tiff(file.path(root_path, '/iProjects/RCheck/GSClassifier/routine01/test/PADv20240810_plotBatchEffect_test01.tif'), width = 40, height = 22, res = 300, units = "cm"); print(p2); dev.off()
     return(p2)
   }
 )
@@ -81,13 +86,11 @@ setMethod(
 #' @importFrom dplyr filter
 #' @importFrom purrr flatten
 #' @importFrom GSClassifier geneMatch
-getGeneMatrix <- function(object, data){
-
+getGeneMatrix <- function(object, data) {
   data_f <- flatten(data)
 
-  data_f2 <- ldply(data_f, function(X){
-
-    if(F){
+  data_f2 <- ldply(data_f, function(X) {
+    if (F) {
       X <- data_f[[1]]
     }
 
@@ -96,20 +99,19 @@ getGeneMatrix <- function(object, data){
       geneAnnotation = object@Repeat$geneAnnotation,
       geneid = object@Repeat$geneid,
       matchmode = c("fix", "free")[1]
-    )[['Subset']]
+    )[["Subset"]]
 
-    if(is.one.true(colMeans(X_subset)>100)){
-      LuckyVerbose('Outlier samples: ', paste(colnames(X_subset), collapse = '; '))
+    if (is.one.true(colMeans(X_subset) > 100)) {
+      LuckyVerbose("Outlier samples: ", paste(colnames(X_subset), collapse = "; "))
     }
 
-    return(cbind(SampleIDs = colnames(X_subset),as.data.frame(t(X_subset))))
-
+    return(cbind(SampleIDs = colnames(X_subset), as.data.frame(t(X_subset))))
   }) %>% filter(!duplicated(SampleIDs))
 
-  data_f3 <- as.matrix(data_f2[,-c(1,2)]); rownames(data_f3) <- as.character(data_f2[,2])
+  data_f3 <- as.matrix(data_f2[, -c(1, 2)])
+  rownames(data_f3) <- as.character(data_f2[, 2])
 
   return(data_f3)
-
 }
 
 
@@ -122,52 +124,54 @@ getGeneMatrix <- function(object, data){
 #' @importFrom plyr llply
 #' @importFrom dplyr arrange filter left_join
 #' @import ggplot2
-plotOneBatchEffect <- function(object,data,mt){
-
+plotOneBatchEffect <- function(object, data, mt) {
   # Reshape data
-  mt <- mt[!duplicated(rownames(mt)),]
+  mt <- mt[!duplicated(rownames(mt)), ]
   mt_df <- cbind(ID = rownames(mt), as.data.frame(mt))
-  mt_df2 <- mt_df %>% melt(id.vars = 'ID', measure.vars = colnames(mt), variable.name = 'variable', value.name = "value")
+  mt_df2 <- mt_df %>% melt(id.vars = "ID", measure.vars = colnames(mt), variable.name = "variable", value.name = "value")
 
   # Annotation
-  if(T){
-    data <- data %>% flatten() %>% llply(.,function(x)colnames(x[['expr']]))
+  if (T) {
+    data <- data %>%
+      flatten() %>%
+      llply(., function(x) colnames(x[["expr"]]))
     annot <- data.frame()
-    for(i in 1:length(data)){ # i=1
-      annot <- rbind(annot,cbind(ID=data[[i]],cohort = names(data)[i]))
+    for (i in 1:length(data)) { # i=1
+      annot <- rbind(annot, cbind(ID = data[[i]], cohort = names(data)[i]))
     }
 
     # Alignment
     coSample <- Reduce(intersect, list(as.character(annot$ID), rownames(mt), names(object@Data$CancerType)))
-    annot <- annot[match(coSample, annot$ID),]
+    annot <- annot[match(coSample, annot$ID), ]
     annot$tissue <- object@Data$CancerType[coSample]
     annot <- annot %>% arrange(tissue, cohort)
-    mt <- mt[coSample,]
+    mt <- mt[coSample, ]
     mt_df2 <- mt_df2 %>% filter(ID %in% coSample)
   }
 
   # Plot data
-  mt_df3 <- left_join(mt_df2, annot, by='ID')
+  mt_df3 <- left_join(mt_df2, annot, by = "ID")
   mt_df3$ID <- factor(mt_df3$ID, levels = rev(annot$ID))
-  nSample <- length(unique(mt_df3$ID)); β = 5/nSample
+  nSample <- length(unique(mt_df3$ID))
+  β <- 5 / nSample
 
   # Here I don't want to use facet() series (～￣▽￣)～
 
   # Plot - Tissue-level
-  if(T){
-
-    p_batch1 <- ggplot(mt_df3,aes(x=ID, y=value, color=tissue, fill='white'))+
+  if (T) {
+    p_batch1 <- ggplot(mt_df3, aes(x = ID, y = value, color = tissue, fill = "white")) +
       geom_boxplot(
         # outlier.shape = NA,  # Hide outliers for clarity
-        outlier.size = 0.001*β,
-        width = 0.006*β, linewidth = 0.001*β) +
-      stat_summary(fun=median, geom="point", shape=23, size=40*β,color='black',fill='black') +
-      labs(x = 'Sample',y = 'Tissue-level') +
+        outlier.size = 0.001 * β,
+        width = 0.006 * β, linewidth = 0.001 * β
+      ) +
+      stat_summary(fun = median, geom = "point", shape = 23, size = 40 * β, color = "black", fill = "black") +
+      labs(x = "Sample", y = "Tissue-level") +
       guides(color = "none", fill = "none") +
       theme_bw() +
       theme(
-        panel.border = element_rect(colour = "black", linewidth=1.5),
-        axis.line = element_line(colour = "black", linewidth=0, linetype = 1),
+        panel.border = element_rect(colour = "black", linewidth = 1.5),
+        axis.line = element_line(colour = "black", linewidth = 0, linetype = 1),
         axis.title = element_text(size = 10),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank()
@@ -177,19 +181,20 @@ plotOneBatchEffect <- function(object,data,mt){
   }
 
   # Plot - Cohort-level
-  if(T){
-    p_batch2 <- ggplot(mt_df3,aes(x=ID, y=value, color=cohort, fill='white'))+
+  if (T) {
+    p_batch2 <- ggplot(mt_df3, aes(x = ID, y = value, color = cohort, fill = "white")) +
       geom_boxplot(
         # outlier.shape = NA,  # Hide outliers for clarity
-        outlier.size = 0.001*β,
-        width = 0.006*β, linewidth = 0.001*β) +
-      stat_summary(fun=median, geom="point", shape=23, size=40*β,color='black',fill='black') +
-      labs(x = 'Sample',y = 'Cohort-level') +
+        outlier.size = 0.001 * β,
+        width = 0.006 * β, linewidth = 0.001 * β
+      ) +
+      stat_summary(fun = median, geom = "point", shape = 23, size = 40 * β, color = "black", fill = "black") +
+      labs(x = "Sample", y = "Cohort-level") +
       guides(color = "none", fill = "none") +
       theme_bw() +
       theme(
-        panel.border = element_rect(colour = "black", linewidth=1.5),
-        axis.line = element_line(colour = "black", linewidth=0, linetype = 1),
+        panel.border = element_rect(colour = "black", linewidth = 1.5),
+        axis.line = element_line(colour = "black", linewidth = 0, linetype = 1),
         axis.title = element_text(size = 10),
         axis.text.x = element_blank(),
         axis.ticks.x = element_blank()
@@ -203,11 +208,8 @@ plotOneBatchEffect <- function(object,data,mt){
   #   align = "v", ncol = 1, rel_heights = c(0.5, 0.5)
   # )
   p_batch <- list()
-  p_batch[['Tissue']] <- p_batch1
-  p_batch[['Cohort']] <- p_batch2
+  p_batch[["Tissue"]] <- p_batch1
+  p_batch[["Cohort"]] <- p_batch2
   # tiff(paste0('./test/',project,'_plotBatch_mt-Matrix_Tissue+Cohort.tif'), width = 20, height = 20, res = 300, units = "cm"); print(p_batch); dev.off()
   return(p_batch)
-
 }
-
-
